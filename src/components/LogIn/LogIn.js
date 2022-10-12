24a4474f9d1e65d86f3301a62c4d6b331c0d6e76
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-10-13 03:30:48 +0600

    responsive for mobile four

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -3,13 +3,13 @@ import './LogIn.css'
 
 const LogIn = () => {
     return (
-        <div className='form ml-[20%] lg:ml-[40%] mt-[5%] w-[220px] py-5 rounded-lg '>
+        <div className='form ml-[20%] lg:ml-[40%] mt-[5%] w-[270px] lg:w-[220px] py-5 rounded-lg '>
         <div>
             <h1 className=''>Log In Form</h1>
             <img className='w-[50px] h-[50px] cursor-pointer rounded-full ml-[35%] mt-5' src="https://cdn5.vectorstock.com/i/thumb-large/90/99/black-thin-line-sign-up-with-hand-vector-28279099.jpg" alt="" />
             <div className='mt-6 '>
-             <input className='block ml-2 lg:ml-3  py-1 px-1 lg:px-2 mb-2' type="text" placeholder='Enter your email address' />
-            <input className='block ml-2 lg:ml-3 py-1 px-1 lg:px-2' type="password" placeholder='Enter your valid password' />
+             <input className='block ml-2 lg:ml-3  py-1 px-2 mb-2' type="text" placeholder='Enter your email address' />
+            <input className='block ml-2 lg:ml-3 py-1 px-2' type="password" placeholder='Enter your valid password' />
             </div>
             <p className='text-gray-600 mt-5'>Forgot password?
              <span className='text-black font-semibold'> or </span>
